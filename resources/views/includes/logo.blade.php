0bba76a75388b8f05705f6a4deed0cd601e33c92
Andreas Ek <andreas@andreasek.se>
2015-10-03 13:30:30 +0200

    Logo 55px

--- resources/views/includes/logo.blade.php
+++ resources/views/includes/logo.blade.php
@@ -1,6 +1,6 @@
     <div class="row">
     	<div class="col-xs-12 text-center">
-	        <h1><a href="/" style="color:#333349;font-size:60px;">Västeråsaren</a></h1>
+	        <h1><a href="/" style="color:#333349;font-size:55px;">Västeråsaren</a></h1>
     	    <hr/>
         </div>
     </div>
